f5372d3e2fde607fc1ac4c9527515a873265def4
kohei23n <kohei.s.n@gmail.com>
2025-06-10 22:42:21 +0900

    updated code for nostalgia

--- test.py
+++ test.py
@@ -1,24 +1,45 @@
 # 1. Importing Dependencies and Data
 ## bringing in tensorflow
 import tensorflow as tf
+import numpy as np
+import os
 
 ## set memory growth for GPUs to avoid memory allocation issues
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-## bringing in tensorflow datasets for fashion mnist
-import tensorflow_datasets as tfds
 ## bringing in matplotlib for visualisation
 from matplotlib import pyplot as plt
 
-## use the tensorflow datasets api to bring in the data source
-ds = tfds.load('fashion_mnist', split='train')
+## bringing in additional imports for custom dataset
+from PIL import Image
 
+## load custom images
+def load_custom_dataset(data_path, img_size=(64, 64)):
+    image_paths = []
+    for file in os.listdir(data_path):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_paths.append(os.path.join(data_path, file))
+    
+    def load_and_preprocess_image(path):
+        image = tf.io.read_file(path)
+        image = tf.image.decode_image(image, channels=3)  # RGB instead of grayscale
+        image.set_shape([None, None, 3])
+        image = tf.image.resize(image, img_size)
+        image = tf.cast(image, tf.float32) / 255.0
+        return image
+    
+    dataset = tf.data.Dataset.from_tensor_slices(image_paths)
+    dataset = dataset.map(load_and_preprocess_image)
+    return dataset
+
+## load custom dataset
+ds = load_custom_dataset('data/frames', img_size=(64, 64))
 
 # 2. Visualise Data and Build Dataset
 ## do some data transformation
-import numpy as np
+
 
 ## setup connection aka iterator
 dataiterator = ds.as_numpy_iterator()
@@ -26,35 +47,22 @@ dataiterator = ds.as_numpy_iterator()
 ## setup the subplot formatting
 fig, ax = plt.subplots(ncols=4, figsize=(20,20))
 
-## loop four times and get images
-for idx in range(4):
-    ## grab an image and label
-    sample = dataiterator.next()
-    ## plot the image using a specific subplot 
-    ax[idx].imshow(np.squeeze(sample['image']))
-    ## appending the image label as the plot title
-    ax[idx].title.set_text(sample['label'])
+## scale images to [-1, 1] range for better GAN training
+def scale_images(image):
+    return (image * 2.0) - 1.0
 
-## show the images
-# plt.show()
-
-## scale and return images only
-def scale_images(data):
-    image = data['image']
-    return image / 255
-
-## reload the dataset
-ds = tfds.load('fashion_mnist', split='train')
+## reload the dataset and apply preprocessing
+ds = load_custom_dataset('data/frames', img_size=(64, 64))
 ## running the dataset through the scale_images preprocessing function
 ds = ds.map(scale_images)
 ## cache the dataset for that batch
 ds = ds.cache()
-## shuffle it up
-ds = ds.shuffle(60000)
-## batch into 512 images per sample
-ds = ds.batch(512)
+## shuffle it up (adjust number based on your dataset size)
+ds = ds.shuffle(1000)  # Reduced from 60000 since you likely have fewer images
+## batch into smaller batches due to larger images
+ds = ds.batch(32)  # Reduced from 512 due to memory constraints
 ## reduces the likelihood of bottlenecking
-ds = ds.prefetch(64)
+ds = ds.prefetch(16)  # Reduced accordingly
 
 
 # 3. Build Neural Network
@@ -67,80 +75,71 @@ from keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, Up
 def build_generator():
     model = Sequential()
     
-    ## takes in random values and reshapes it to 7x7x128
+    ## takes in random values and reshapes it to 8x8x256 for 64x64 output
     ## beginnings of a generated image
-    model.add(Dense(7*7*128, input_dim=128))
+    model.add(Dense(8*8*256, input_dim=128))
     model.add(LeakyReLU(0.2))
-    model.add(Reshape((7, 7, 128)))
+    model.add(Reshape((8, 8, 256)))
 
-    ## upsampling block 1
+    ## upsampling block 1 - 8x8 to 16x16
     model.add(UpSampling2D())
-    model.add(Conv2D(128, 5, padding='same'))
+    model.add(Conv2D(128, 3, padding='same'))
     model.add(LeakyReLU(0.2))
 
-    ## upsampling block 2
+    ## upsampling block 2 - 16x16 to 32x32
     model.add(UpSampling2D())
-    model.add(Conv2D(128, 5, padding='same'))
+    model.add(Conv2D(64, 3, padding='same'))
     model.add(LeakyReLU(0.2))
 
-    ## convolutional block 1
-    model.add(Conv2D(128, 4, padding='same'))
+    ## upsampling block 3 - 32x32 to 64x64
+    model.add(UpSampling2D())
+    model.add(Conv2D(32, 3, padding='same'))
     model.add(LeakyReLU(0.2))
 
-    ## convolutional block 2
-    model.add(Conv2D(128, 4, padding='same'))
+    ## convolutional block for refinement
+    model.add(Conv2D(32, 3, padding='same'))
     model.add(LeakyReLU(0.2))
 
-    ## conv layer to get to one channel
-    model.add(Conv2D(1, 4, padding='same', activation='sigmoid'))
+    ## conv layer to get to three channels (RGB) with tanh activation
+    model.add(Conv2D(3, 3, padding='same', activation='tanh'))
 
     return model
 
 generator = build_generator()
 
-## generate new fashion
+## generate new nostalgic images
 img = generator.predict(np.random.randn(4, 128, 1))
 print(img.shape)
 
 ## setup the subplot formatting
 fig, ax = plt.subplots(ncols=4, figsize=(20,20))
 
-## loop four times and get images
-for idx, single_img in enumerate(img):
-    ## plot the image using a specific subplot 
-    ax[idx].imshow(np.squeeze(single_img))
-    ## appending the image label as the plot title
-    ax[idx].title.set_text(idx)
-
-## show the images
-# plt.show()
-
 def build_discriminator():
     model = Sequential()
 
-    ## convolutional block 1
-    model.add(Conv2D(32, 5, input_shape=(28, 28, 1)))
+    ## convolutional block 1 - now expects 64x64x3 RGB images
+    model.add(Conv2D(32, 4, strides=2, padding='same', input_shape=(64, 64, 3)))
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
-    ## convolutional block 2
-    model.add(Conv2D(64, 5))
+    ## convolutional block 2 - 32x32
+    model.add(Conv2D(64, 4, strides=2, padding='same'))
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
-    ## convolutional block 3
-    model.add(Conv2D(128, 5))
+    ## convolutional block 3 - 16x16
+    model.add(Conv2D(128, 4, strides=2, padding='same'))
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
-    ## convolutional block 4
-    model.add(Conv2D(256, 5))
+    ## convolutional block 4 - 8x8
+    model.add(Conv2D(256, 4, strides=2, padding='same'))
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
     ## flatten then pass to dense layer
     model.add(Flatten())
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
     model.add(Dense(1, activation='sigmoid'))
 
     return model
@@ -165,7 +164,7 @@ d_loss = BinaryCrossentropy()
 ## importing the base model class to subclass our training step
 from keras.models import Model
 
-class FashionGAN(Model):
+class NostalgiaGAN(Model):
     def __init__(self, generator, discriminator, *args, **kwargs):
         ## pass throguh args and kwargs to base class
         super().__init__(*args, **kwargs)
@@ -187,7 +186,7 @@ class FashionGAN(Model):
     def train_step(self, batch):
         ## get the data
         real_images = batch
-        fake_images = self.generator(tf.random.normal((128, 128, 1)), training=False)
+        fake_images = self.generator(tf.random.normal((32, 128, 1)), training=False)
 
         ## train the discriminator
         with tf.GradientTape() as d_tape:
@@ -214,7 +213,7 @@ class FashionGAN(Model):
         ## train the generator
         with tf.GradientTape() as g_tape:
             ## generate some new images
-            gen_images = self.generator(tf.random.normal((128, 128, 1)), training=True)
+            gen_images = self.generator(tf.random.normal((32, 128, 1)), training=True)
 
             ## create the predicted labels
             predicted_labels = self.discriminator(gen_images, training=False)
@@ -232,16 +231,17 @@ class FashionGAN(Model):
         }
 
 ## create instance of subclassed model
-fashgan = FashionGAN(generator, discriminator)
+nostalgiagan = NostalgiaGAN(generator, discriminator)
 
 ## compile the model
-fashgan.compile(g_opt, d_opt, g_loss, d_loss)
+nostalgiagan.compile(g_opt, d_opt, g_loss, d_loss)
 
-## build calllback
-import os
-# from keras.preprocessing.image import array_to_img
+## from keras.preprocessing.image import array_to_img
 from keras.utils.image_utils import array_to_img
 from keras.callbacks import Callback
+
+## make sure the images directory exists
+os.makedirs('images', exist_ok=True)
 
 class ModelMonitor(Callback):
     def __init__(self, num_img=3, latent_dim=128):
@@ -251,8 +251,10 @@ class ModelMonitor(Callback):
     def on_epoch_end(self, epoch, logs=None):
         random_latent_vectors = tf.random.normal((self.num_img, self.latent_dim, 1))
         generated_images = self.model.generator(random_latent_vectors)
-        generated_images *= 255
-        generated_images.numpy()
+        # Convert from [-1,1] to [0,1] then to [0,255] for saving
+        generated_images = (generated_images + 1) / 2.0
+        generated_images = generated_images * 255
+        generated_images = generated_images.numpy()
         for i in range(self.num_img):
             img = array_to_img(generated_images[i])
             img.save(os.path.join('images', f'generated_img_{epoch}_{i}.png'))
@@ -274,7 +276,7 @@ checkpoint_callback = ModelCheckpoint(
 )
 
 ## train
-hist = fashgan.fit(ds, epochs=400, callbacks=[ModelMonitor(), checkpoint_callback])
+hist = nostalgiagan.fit(ds, epochs=400, callbacks=[ModelMonitor(), checkpoint_callback])
 
 
 ## review performance
@@ -288,7 +290,10 @@ plt.show()
 # 5. Test Out the Generator
 
 imgs = generator.predict(tf.random.normal((16, 128, 1)))
+# Convert from [-1,1] back to [0,1] for display
+imgs = (imgs + 1) / 2.0
 fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 20))
 for r in range(4):
     for c in range(4):
         ax[r][c].imshow(imgs[(r+1)*(c+1)-1])
+        ax[r][c].axis('off')
